4403ba527d1a5ed0ad94327a7e1d4c464dbb3c0b
Kaleb <Kalebreid15@gmail.com>
2025-06-17 23:34:31 -0400

    Having issues with error code for ambiguity

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        // Constructor to increment the static numberOfCars field in CarLot  
+        public Car()
+        {
+            CarLot.numberOfCars++;
+        }
+    }
+    public class CarLot
+    {
+        // Static field to track the number of cars  
+        public static int numberOfCars = 0;
+
+        // Property to hold a list of cars in the parking lot  
+        public List<Car> ParkingLot { get; set; } = new List<Car>();
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,77 +1,71 @@
-﻿using System;
+﻿using CarLotSimulator;
+using System;
 using System.Collections.Generic;
+
+var carLot = new CarLot();
+
+// Car 1 - Object Initializer  
+Car car1 = new Car()
+{
+    Year = 2020,
+    Make = "Toyota",
+    Model = "Camry",
+    EngineNoise = "Vroom Vroom",
+    HonkNoise = "Beep Beep",
+    IsDriveable = true
+};
+carLot.ParkingLot.Add(car1);
+CarLot.NumberOfCars++;
+
+// Car 2 - Object Initializer  
+Car car2 = new Car()
+{
+    Year = 2018,
+    Make = "Honda",
+    Model = "Civic",
+    EngineNoise = "Silent Buzz",
+    HonkNoise = "Meep",
+    IsDriveable = true
+};
+carLot.ParkingLot.Add(car2);
+CarLot.NumberOfCars++;
+
+// Car 3 - Object Initializer  
+Car car3 = new Car()
+{
+    Year = 2021,
+    Make = "Ford",
+    Model = "Mustang",
+    EngineNoise = "Clatter",
+    HonkNoise = "Beep Beep",
+    IsDriveable = true
+};
+carLot.ParkingLot.Add(car3);
+CarLot.NumberOfCars++;
+
+// Display car details and noises  
+DisplayCarInfo(car1);
+DisplayCarInfo(car2);
+DisplayCarInfo(car3);
+
+// Final Inventory Output  
+Console.WriteLine("\n--- Full Car Lot Inventory ---");
+foreach (var car in carLot.ParkingLot)
+{
+    Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+}
+
+Console.WriteLine($"\nTotal number of cars: {CarLot.NumberOfCars}");
+
+void DisplayCarInfo(Car car)
+{
+    Console.WriteLine($"\nCar: {car.Year} {car.Make} {car.Model}");
+    car.MakeEngineNoise(car.EngineNoise);
+    car.MakeHonkNoise(car.HonkNoise);
+}
 
 namespace CarLotSimulator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Car 1
-            Car car1 = new Car();
-            car1.Year = 2020;
-            car1.Make = "Toyota";
-            car1.Model = "Camry";
-            car1.EngineNoise = "Vroom Vroom";
-            car1.HonkNoise = "Beep Beep";
-            car1.IsDriveable = true;
-
-            //Car 2
-            Car car2 = new Car();
-            car2.Year = 2018;
-            car2.Make = "Honda";
-            car2.Model = "Civic";
-            car2.EngineNoise = "Silent Buzz";
-            car2.HonkNoise = "Meep";
-            car2.IsDriveable = true;
-
-            //Car 3
-            Car car3 = new Car();
-            car3.Year = 2021;
-            car3.Make = "Ford";
-            car3.Model = "Mustang";
-            car3.EngineNoise = "Clatter";
-            car3.HonkNoise = "Beep Beep";
-            car3.IsDriveable = true;
-
-
-            // Call method for each car 
-
-            Console.WriteLine($"Car 1: {car1.Year} {car1.Make} {car1.Model}");
-            car1.MakeEngineNoise(car1.EngineNoise);
-            car1.MakeHonkNoise(car1.HonkNoise);
-
-            Console.WriteLine($"\nCar 2: {car2.Year} {car2.Make} {car2.Model}");
-            car2.MakeEngineNoise(car2.EngineNoise);
-            car2.MakeHonkNoise(car2.HonkNoise);
-
-            Console.WriteLine($"\nCar 3: {car3.Year} {car3.Make} {car3.Model}");
-            car3.MakeEngineNoise(car3.EngineNoise);
-            car3.MakeHonkNoise(car3.HonkNoise);
-
-
-            //TODO
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable 
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
-
-
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
-
-            //*************BONUS*************//
-
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
-
-            //*************BONUS X 2*************//
-
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-        }
-    }
     public class Car
     {
         public int Year { get; set; }
@@ -85,10 +79,16 @@ namespace CarLotSimulator
         {
             Console.WriteLine($"The engine noise is: {noise}");
         }
+
         public void MakeHonkNoise(string noise)
         {
             Console.WriteLine($"The honk noise is: {noise}");
         }
     }
+
+    public class CarLot
+    {
+        public List<Car> ParkingLot { get; set; } = new List<Car>();
+        public static int NumberOfCars = 0;
+    }
 }
-// CarLot class to hold a list of cars
